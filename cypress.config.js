31c9e64104d820feeb4206f96c4ca51aca9fdb8a
Elshauna Dixon <shaunadixon1@gmail.com>
2022-12-04 11:28:48 -0500

    authenthication

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    "chromeWebSecurity":false,
+    baseUrl:'https://ui-automation-camp.vercel.app/',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
   },
 });
