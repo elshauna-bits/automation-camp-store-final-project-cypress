1eed5e9f6803431265f373f292f571db95766dfa
Elshauna Dixon <shaunadixon1@gmail.com>
2022-12-05 10:14:22 -0500

    adding comments to authentication.cy.js

--- cypress/e2e/spec/authentication.cy.js
+++ cypress/e2e/spec/authentication.cy.js
@@ -2,39 +2,33 @@ import auth from "../page/auth"
 describe('Authenthication',()=>{
     beforeEach(()=>{
         cy.visit('/')
+        auth.signInOrRegister()// selects the Sign/Register button on the landing page
     })
     it('should create an account for the application',()=>{
-       auth.signInOrRegister()
-       auth.signUp('pain@mail.com','P@$$w0rd!')
+       auth.signUp('pain@mail.com','P@$$w0rd!')// registering a new account
     })
     it('should login into the application',()=>{
-        auth.signInOrRegister()
         auth.login("pain@mail.com","P@$$w0rd!")
-        //cy.url().should('eq','https://ui-automation-camp.vercel.app/products')
+        cy.url().should('eq','https://ui-automation-camp.vercel.app/products')// asserts that the user is taken to the products page after login
     })
     it('should not login with incorrect password',()=>{
-        auth.signInOrRegister()
-        auth.login("ellen@mail.com","P@$$w0rd")
-        cy.get(auth.errorMessage).should('have.text','Wrong email or password.')
+        auth.login("ellen@mail.com","P@$$w0rd") // attempts to login with an incorrect password
+        cy.get(auth.errorMessage).should('have.text','Wrong email or password.') // asserts that the user is prompted with an error message
     })
     it('should not login with incorrect email',()=>{
-        auth.signInOrRegister()
-        auth.login("ell77en@mail.com","P@$$w0rd!")
-        cy.get(auth.errorMessage).should('have.text','Wrong email or password.')
+        auth.login("ell77en@mail.com","P@$$w0rd!") // attempts to login with an incorrect email 
+        cy.get(auth.errorMessage).should('have.text','Wrong email or password.') // asserts that the user is prompted with an error message
     })
     it('should not login with invalid email',()=>{
-        auth.signInOrRegister()
-        auth.login("e7en@mail","P@$$w0rd!")
-        cy.get(auth.invalidCredError).should('have.text','Email is invalid')
+        auth.login("e7en@mail","P@$$w0rd!") //attempts to login with an invalid email
+        cy.get(auth.invalidCredError).should('have.text','Email is invalid') // asserts that the user is prompted with an error message
     })
     it('should not create an account for the application without password',()=>{
-        auth.signInOrRegister()
-        auth.signUpWithoutPassword('ellen@mail.com')
-        cy.get(auth.invalidCredError).should('have.text',"Password can't be blank")
+        auth.signUpWithoutPassword('ellen@mail.com') // attempts to create an account without a password
+        cy.get(auth.invalidCredError).should('have.text',"Password can't be blank") // asserts that the user is prompted with an error message
     })
     it('should not login without password',()=>{
-        auth.signInOrRegister()
-        auth.loginWithoutPassword("pain@mail.com")
-        cy.get(auth.invalidCredError).should('have.text',"Password can't be blank")
+        auth.loginWithoutPassword("pain@mail.com") // attempts to login without password
+        cy.get(auth.invalidCredError).should('have.text',"Password can't be blank") // asserts that the user is prompted with an error message
     })
 })
