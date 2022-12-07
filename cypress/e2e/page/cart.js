5ad1d210ff206a75c78f059494b1688ac3741b0f
Elshauna Dixon <shaunadixon1@gmail.com>
2022-12-06 19:10:53 -0500

    cart

---
+++ cypress/e2e/page/cart.js
@@ -0,0 +1,35 @@
+class Cart{
+    get cartIcon(){
+      return('#top-cart')  
+    }
+    get increaseQuantity(){
+        return('button[title="Increment quantity"]')
+    }
+    get decreaseQuantity(){
+        return ('button[title="Decrement quantity"]')
+    }
+    get deleteItem(){
+        return('button[title="Remove item"]')
+    }
+    get itemAmount(){
+        return('.snipcart__font--secondary.snipcart__font--regular')
+    }
+    get cartItem(){
+        return('.snipcart-item-line__product')
+    }
+
+    increaseItemQuantity(){
+       cy.wait(6000)
+       cy.get(this.cartIcon).click()
+       cy.get(this.increaseQuantity).click() 
+    }
+    decreaseItemQuantity(){
+        cy.get(this.cartIcon).click()
+        cy.get(this.decreaseQuantity).click()
+    }
+    deleteItemFromCart(){
+        cy.get(this.cartIcon).click()
+        cy.get(this.deleteItem).click()
+    }
+}
+export default new Cart()
---
+++ cypress/e2e/spec/cart.cy.js
@@ -0,0 +1,19 @@
+import auth from "../page/auth"
+import products from "../page/products"
+import cart from "../page/cart"
+
+describe('Cart interactions', ()=>{
+    beforeEach(()=>{
+        cy.visit('/')
+        auth.signInOrRegister()
+        //auth.login("pain@mail.com","P@$$w0rd!")
+        cy.url().should('eq','https://ui-automation-camp.vercel.app/products')
+    })
+
+    it('should increase item quantity',()=>{
+        products.addOneItemToCart()
+        cart.increaseItemQuantity()
+        cy.get(cart.itemAmount).should('have.text','2')
+    })
+
+})
