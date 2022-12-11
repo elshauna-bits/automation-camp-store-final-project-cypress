45eb3766fcc433dcba94dfeaa065ded4df48d948
Elshauna Dixon <shaunadixon1@gmail.com>
2022-12-11 13:52:04 -0500

    product-details

--- cypress/e2e/page/prod-details.js
+++ cypress/e2e/page/prod-details.js
@@ -1,3 +1,28 @@
 class Details{
+get qualityMug(){
+    return(`img[src="/images/quality-mug.jpg"]`)
+}
+get mugTitle(){
+    return(`//h2[contains(text(),'Mug')]`)
+}
+get mugDescription(){
+    return("//p[contains(text(),'Wampumtuk Pack of 2 Company Policies Boss Employee')]")
+}
+get mugPrice(){
+    return('p+div')
+}
+get category(){
+    return('.css-1ccau2i')
+}
+get backBtn(){
+    return('.chakra-heading.css-18j379d')
+}
+
+viewQualityMugPage(){
+    cy.get(this.qualityMug).click()
+}
+goBacktoProducts(){
+    cy.get(this.backBtn).click()
+}
 
 }export default new Details
