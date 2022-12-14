98310e8aebcc521109e25dd007b7a88d605d706d
Elshauna Dixon <shaunadixon1@gmail.com>
2022-12-13 20:07:29 -0500

    filter and sort

---
+++ cypress/e2e/page/sort.js
@@ -0,0 +1,5 @@
+class Sort{
+get productNames(){
+    return("p[class='chakra-text css-1n64n71']")
+}
+}export default new Sort()
---
+++ cypress/e2e/spec/filter-and-sort.cy.js
@@ -0,0 +1,46 @@
+import auth from "../page/auth"
+import products from "../page/products"
+import sort from "../page/sort"
+
+describe('Filter and sort products',()=>{
+    beforeEach(()=>{
+        cy.visit('/')
+        auth.signInOrRegister()
+        //auth.login("pain@mail.com","P@$$w0rd!")
+        cy.url().should('eq','https://ui-automation-camp.vercel.app/products')
+    })
+
+    it('should sort from A to Z',()=>{
+        cy.get(products.sortSelect).select('aToZ')
+        const qualityProducts = ['Quality Stylish Shoes','Dell Laptop','Gray Couch','HP Laptop','Mackbook Pro','Quality Blue Shoes','Quality Cargo Pants','Quality Fitted Hat',
+        'Quality Heal Shoes','Quality Hooded Sweatshirt','Quality Jeans Pants','Quality Kids Tshirt','Quality Trucker Hat','Quality Mousepad','Quality Mug','Quality Pillow',
+        'Quality Pink Pants','Red Couch','Quality Sweatshirt','Quality Tshirt','Quality Vneck','White Couch']
+        qualityProducts.sort()
+        cy.wait(5000)
+
+        cy.get(sort.productNames).each(($elem,index) =>{
+            expect($elem.text()).equal(qualityProducts[index])
+        })
+    })
+
+    it('should sort from Z to A',()=>{
+        cy.get(products.sortSelect).select('zToA')
+        const qualityProducts = ['Dell Laptop','Gray Couch','HP Laptop','Mackbook Pro','Quality Blue Shoes','Quality Cargo Pants','Quality Fitted Hat',
+        'Quality Heal Shoes','Quality Hooded Sweatshirt','Quality Jeans Pants','Quality Kids Tshirt','Quality Mousepad','Quality Mug','Quality Pillow',
+        'Quality Pink Pants','Quality Stylish Shoes','Quality Sweatshirt','Quality Trucker Hat','Quality Tshirt','Quality Vneck','Red Couch','White Couch']
+        qualityProducts.sort().reverse()
+        cy.wait(5000)
+        cy.get(sort.productNames).each(($elem,index) =>{
+            expect($elem.text()).equal(qualityProducts[index])
+        })
+    })
+
+    it('should filter by category',()=>{
+        cy.get(products.categorySelect).select('laptop') // selects the laptop option in the category field
+        const laptops = ["Mackbook Pro","Dell Laptop","HP Laptop"]
+        cy.wait(5000)
+        cy.get(sort.productNames).each(($elem,index) =>{
+            expect($elem.text()).equal(laptops[index])
+        })
+    })
+})
