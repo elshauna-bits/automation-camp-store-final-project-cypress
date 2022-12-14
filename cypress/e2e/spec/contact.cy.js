e4af31458cb9c84a32fa850a19411e87c6043feb
Elshauna Dixon <shaunadixon1@gmail.com>
2022-12-14 18:54:02 -0500

    contact

---
+++ cypress/e2e/spec/contact.cy.js
@@ -0,0 +1,9 @@
+describe('Contact Page',()=>{
+    beforeEach(()=>{
+        cy.visit('/')
+        auth.signInOrRegister()
+        //auth.login("pain@mail.com","P@$$w0rd!")
+        cy.url().should('eq','https://ui-automation-camp.vercel.app/products')
+    })
+    
+})
