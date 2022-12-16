0cc21d19be275e16f9eeee9f794460f0ce36ea56
Elshauna Dixon <shaunadixon1@gmail.com>
2022-12-15 21:56:35 -0500

    updated contact beforeEach

--- cypress/e2e/spec/contact.cy.js
+++ cypress/e2e/spec/contact.cy.js
@@ -3,27 +3,16 @@ import contact from "../page/contact-page"
 
 describe('Contact Page',()=>{
     beforeEach(()=>{
-        cy.visit('/')
-        cy.get('#signInOrRegister').click();
-
-        cy.origin(
-            "https://dev-mlluudmotpwoldtv.us.auth0.com",
-            { args: {} },
-            ({}) => {
-                cy.get('li[class="auth0-lock-tabs-current"]');
-                cy.get('input[name="email"]').type('annxsivan@gmail.com');
-                cy.get('input[name="password"]').type('P@ssword12');
-                cy.get('button[id="1-submit"]').click();
-                }
-        );  
+        auth.login()
+        cy.wait(6000) 
         cy.get(contact.contactPage).click()
         cy.wait(5000)
     })
-    xit('should verify that the email address is correct',()=>{
+    it('should verify that the email address is correct',()=>{
 
         cy.get(contact.emailAddress).should('have.text','info@qualityworkscg.com')// should verify that the email address on the contact page is correct
     })
-    xit('should send a message',()=>{
+    it('should send a message',()=>{
         contact.sendMessage("Ky","Levy","kylev@mail.com","Bootcamp","When does it start?")// sends  a message 
         cy.wait(5000)
         cy.get(contact.confirmation).should('be.visible')//checks if the message sent notification is visiblw
