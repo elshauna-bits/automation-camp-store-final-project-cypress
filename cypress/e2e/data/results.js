506da9f1ca95c474f29bbe76f8aa59e8caceb7e7
Elshauna Dixon <shaunadixon1@gmail.com>
2022-12-15 21:10:03 -0500

    data-driven sorting

---
+++ cypress/e2e/data/results.js
@@ -0,0 +1,17 @@
+module.exports = {
+  
+    laptops: 
+    [{
+        name: 'Mackbook Pro',
+        price: '2000'
+    },
+    
+    {
+        name:'Dell Laptop',
+        price:'1000'
+    },
+    {
+        name:'HP Laptop',
+        price:'680'
+    }]
+}
